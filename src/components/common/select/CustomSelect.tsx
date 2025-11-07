42fcd6e0ecfb36224b735e458dafab60c789252b
치훈 <rlaclgns321@naver.com>
2025-11-07 21:19:04 +0900

    Fix: 에러 유발 코드 임시 주석 처리

--- src/components/common/select/CustomSelect.tsx
+++ src/components/common/select/CustomSelect.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormControl, MenuItem, Select } from "@mui/material";
+// import { FormControl, MenuItem, Select } from "@mui/material";
 
 interface CustomSelectOption {
   label: string;
@@ -17,57 +17,60 @@ interface CustomSelectProps {
   isActive?: boolean;
 }
 
-export default function CustomSelect({
-  value,
-  options,
-  onChange,
-  disabled = false,
-  placeholder = "선택하세요",
-  className = "",
-  isActive = false,
-}: CustomSelectProps) {
+export default function CustomSelect(
+  {
+    // value,
+    // options,
+    // onChange,
+    // disabled = false,
+    // placeholder = "선택하세요",
+    // className = "",
+    // isActive = false,
+  }: CustomSelectProps,
+) {
   return (
-    <FormControl fullWidth size="small" disabled={disabled}>
-      <Select
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        displayEmpty
-        className={`rounded ${className}`}
-        MenuProps={{
-          disablePortal: true,
-        }}
-        sx={{
-          "& .MuiSelect-select": {
-            borderRadius: "4px",
-            transition: "background-color 0.2s",
-            backgroundColor: isActive ? "#ffce88" : "#e4e4e6",
-            color: disabled ? "#a6a6a6" : "#000000",
-            "&:hover": {
-              backgroundColor: disabled ? "#e4e4e6" : "#f6f6f9",
-            },
-          },
-          "&.Mui-focused .MuiSelect-select": {
-            backgroundColor: "#ffce88",
-          },
-          "&.Mui-disabled .MuiSelect-select": {
-            backgroundColor: "#e4e4e6",
-            color: "#a6a6a6",
-            pointerEvents: "none",
-          },
-          "& fieldset": {
-            border: "none",
-          },
-        }}
-      >
-        <MenuItem value="">
-          <span className="text-gray-400">{placeholder}</span>
-        </MenuItem>
-        {options.map((opt) => (
-          <MenuItem key={opt.value} value={opt.value}>
-            {opt.label}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+    // <FormControl fullWidth size="small" disabled={disabled}>
+    //   <Select
+    //     value={value}
+    //     onChange={(e) => onChange(e.target.value)}
+    //     displayEmpty
+    //     className={`rounded ${className}`}
+    //     MenuProps={{
+    //       disablePortal: true,
+    //     }}
+    //     sx={{
+    //       "& .MuiSelect-select": {
+    //         borderRadius: "4px",
+    //         transition: "background-color 0.2s",
+    //         backgroundColor: isActive ? "#ffce88" : "#e4e4e6",
+    //         color: disabled ? "#a6a6a6" : "#000000",
+    //         "&:hover": {
+    //           backgroundColor: disabled ? "#e4e4e6" : "#f6f6f9",
+    //         },
+    //       },
+    //       "&.Mui-focused .MuiSelect-select": {
+    //         backgroundColor: "#ffce88",
+    //       },
+    //       "&.Mui-disabled .MuiSelect-select": {
+    //         backgroundColor: "#e4e4e6",
+    //         color: "#a6a6a6",
+    //         pointerEvents: "none",
+    //       },
+    //       "& fieldset": {
+    //         border: "none",
+    //       },
+    //     }}
+    //   >
+    //     <MenuItem value="">
+    //       <span className="text-gray-400">{placeholder}</span>
+    //     </MenuItem>
+    //     {options.map((opt) => (
+    //       <MenuItem key={opt.value} value={opt.value}>
+    //         {opt.label}
+    //       </MenuItem>
+    //     ))}
+    //   </Select>
+    // </FormControl>
+    <></>
   );
 }
