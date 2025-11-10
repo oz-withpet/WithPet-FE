3e4b898374135ef9bbe0e13820e72b241f0dc0fd
agrade1 <kigf59@gmail.com>
2025-11-10 16:12:51 +0900

    Feat: tanstack query provider 셋팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/layout/Header";
 import CommunityNav from "@/features/community/CommunityNav";
 import { MyPageMainNav, MyPagePostNav, MyPageProfileNav } from "@/features/mypage/MyPageNav";
 import MSWProvider from "@/providers/MSWProvider";
+import QueryProvider from "@/providers/QueryProvider";
 import ReduxProvider from "@/shared/store/ReduxProvider";
 
 export default function RootLayout({
@@ -19,14 +20,16 @@ export default function RootLayout({
     <html lang="ko">
       <body className="bg-background text-gray-100">
         <ReduxProvider>
-          <MSWProvider>
-            <Header />
-            {pathname.includes("community") && <CommunityNav />}
-            {pathname.includes("mypage") && <MyPageMainNav />}
-            {pathname.includes("mypage/profile") && <MyPageProfileNav />}
-            {pathname.includes("mypage/posts") && <MyPagePostNav />}
-            <main className="m-auto w-full max-w-layout py-20">{children}</main>
-          </MSWProvider>
+          <QueryProvider>
+            <MSWProvider>
+              <Header />
+              {pathname.includes("community") && <CommunityNav />}
+              {pathname.includes("mypage") && <MyPageMainNav />}
+              {pathname.includes("mypage/profile") && <MyPageProfileNav />}
+              {pathname.includes("mypage/posts") && <MyPagePostNav />}
+              <main className="m-auto w-full max-w-layout py-20">{children}</main>
+            </MSWProvider>
+          </QueryProvider>
         </ReduxProvider>
       </body>
     </html>
