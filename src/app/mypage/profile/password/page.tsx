d99e593da9dc40cef2dc132edf8492f948bd11df
치훈 <rlaclgns321@naver.com>
2025-11-10 13:03:43 +0900

    Feat: 프로필·프로필 수정 UI 구현

--- src/app/mypage/profile/password/page.tsx
+++ src/app/mypage/profile/password/page.tsx
@@ -1,4 +1,4 @@
-import PasswordChangeForm from "@/features/mypage/password-change/ui/PasswordChangeForm";
+import PasswordChangeForm from "@/features/mypage/profile/password/ui/PasswordChangeForm";
 
 export default function PasswordChangePage() {
   return (
