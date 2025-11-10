83391b139d82455a35b9fc46dc4ca8f33184ca63
치훈 <rlaclgns321@naver.com>
2025-11-10 16:05:07 +0900

    Chore: unused MyPageProfileNav 컴포넌트 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { usePathname } from "next/navigation";
 import CommonMainNav from "@/components/common/nav/CommonMainNav";
 import CommonSubNav from "@/components/common/nav/CommonSubNav";
 import Header from "@/components/layout/Header";
-import MyPageProfileNav from "@/features/mypage/ProfileNav";
 import MSWProvider from "@/providers/MSWProvider";
 import QueryProvider from "@/providers/QueryProvider";
 import ReduxProvider from "@/shared/store/ReduxProvider";
@@ -26,7 +25,6 @@ export default function RootLayout({
               <Header />
               {pathname.includes("community") && <CommonMainNav page="community" />}
               {pathname.includes("mypage") && <CommonSubNav />}
-              {pathname.includes("mypage/profile/") && <MyPageProfileNav />}
               {pathname.includes("mypage/posts") && <CommonMainNav page="mypage" />}
               <main className="m-auto w-full max-w-layout">{children}</main>
             </MSWProvider>
