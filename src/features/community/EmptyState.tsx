241e0068f97e5fcaee27180fc4c31d7b1a537c94
치훈 <rlaclgns321@naver.com>
2025-11-13 16:02:11 +0900

    Feat: 게시글 없음 UI 구현 및 EmptyState 컴포넌트 props 추가

--- src/features/community/EmptyState.tsx
+++ src/features/community/EmptyState.tsx
@@ -1,8 +1,10 @@
-export default function EmptyState() {
+type EmptyStateProps = { content: string };
+
+export default function EmptyState({ content }: EmptyStateProps) {
   return (
-    <div className="w-wrapper flex h-[820px] flex-col items-center justify-center bg-orange-100">
+    <div className="flex h-[820px] w-wrapper flex-col items-center justify-center bg-orange-100">
       <div className="mb-14 text-xl font-bold text-gray-900">WithPet</div>
-      <div className="text-2xl font-bold text-orange-600">작성한 게시글이 없습니다.</div>
+      <div className="text-2xl font-bold text-orange-600">{content}</div>
     </div>
   );
 }
--- src/features/community/ui/CommunityShell.tsx
+++ src/features/community/ui/CommunityShell.tsx
@@ -1,25 +1,22 @@
+import { DUMMY_POST_DETAILS } from "@/mocks/data/postDetails";
 import { Category } from "@/types/category";
 
 import PostItem from "../../../components/common/cards/PostItem";
+import EmptyState from "../EmptyState";
 
 export default function CommunityShell({ category }: { category: Category }) {
   return (
     <div className="mx-auto flex w-main flex-col items-center">
       <div className="flex w-main flex-col items-center">
-        {Array(4)
-          .fill("")
-          .map((el) => (
-            <PostItem
-              key={el}
-              category={category}
-              title="제목"
-              content=""
-              user="작성자"
-              comment={0}
-              ago={0}
-            />
-          ))}
-        {/* <EmptyState /> 게시글 없거나 검색 결과 없을 경우 props로 활용*/}
+        {DUMMY_POST_DETAILS.length ? (
+          <>
+            {DUMMY_POST_DETAILS.map((el) => (
+              <PostItem key={el.id} {...el} />
+            ))}
+          </>
+        ) : (
+          <EmptyState content="게시글 없습니다." />
+        )}
       </div>
     </div>
   );
