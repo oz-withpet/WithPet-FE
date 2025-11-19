196b39d509b532f1e5b360edb26b5ca178ba0287
agrade1 <kigf59@gmail.com>
2025-11-20 04:32:36 +0900

    Style: 지도 레이아웃 여백 추가

--- src/app/(public)/map/page.tsx
+++ src/app/(public)/map/page.tsx
@@ -2,7 +2,7 @@ import { MapLayout } from "@/features/map";
 
 export default function MapPage() {
   return (
-    <div className="flex-center relative">
+    <div className="flex-center relative my-20">
       <MapLayout />
     </div>
   );
